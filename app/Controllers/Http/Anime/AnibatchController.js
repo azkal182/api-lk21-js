e877859a9571f50038da6e6314c57e05ad0e9b47
azka <mohazkalarif"gmail.com>
2022-12-09 02:37:09 +0700

    fix type

--- app/Controllers/Http/Anime/AnibatchController.js
+++ app/Controllers/Http/Anime/AnibatchController.js
@@ -36,7 +36,7 @@ class AnibatchController {
         const get_list_thumb = $(this).find("img").attr("srcset")
         const word = get_list_thumb.split(',')
         let size_thumb = []
-        word.forEach(function(v, i) {
+        word.forEach(function (v, i) {
           // const newData = JSON.parse(v.trim().split(' ')[0])
           //size_thumb.push(newData)
           size_thumb.push(v.trim().split(' ')[0])
@@ -58,7 +58,7 @@ class AnibatchController {
       //console.log(index)
       return {
         message: 'success', count: index.length, results:
-        index
+          index
       }
     });
 
@@ -78,10 +78,11 @@ class AnibatchController {
       }
     }
 
-    let result = await axios.get(host+id, config).then((res) => {
+    let result = await axios.get(host + id, config).then((res) => {
       const html = res.data
       const $ = cheerio.load(html)
       const data = $(".entry-content")
+
       let meta = {
         title: '',
         type: '',
@@ -96,8 +97,12 @@ class AnibatchController {
 
       }
       meta['title'] = data.find('ul > li:nth-child(1)').text().split(':')[1]
-      meta['type'] = data.find('ul > li:nth-child(3)').text().split(':')[1]
-      meta['status'] = data.find('ul > li:nth-child(4)').text().split(':')[1]
+      // meta['type'] = data.find('ul > li:nth-child(3)').text().split(':')[1]
+      meta['type'] = data.find('ul > li:nth-child(3) > a').html()
+      // console.log(data.find('ul > li:nth-child(3) > a').html())
+      // meta['status'] = data.find('ul > li:nth-child(4)').text().split(':')[1]
+      meta['status'] = data.find('ul > li:nth-child(4)').html().replaceAll(/<.?strong>/gmi, "").split(':')[1].trim()
+      // console.log(data.find('ul > li:nth-child(4)').html().replaceAll(/<.?strong>/gmi, "").split(':')[1].trim())
       meta['total_episode'] = data.find('ul > li:nth-child(5)').text().split(':')[1]
       meta['release'] = data.find('ul > li:nth-child(6)').text().split(':')[1]
       meta['broadcast_date'] = data.find('ul > li:nth-child(7)').text().split(':')[1]
@@ -109,7 +114,8 @@ class AnibatchController {
       //meta['genres'] = data.find('ul > li:nth-child(10)>a').text()
 
       let cek = {
-        genre: []}
+        genre: []
+      }
       //console.log(genre.length)
 
       genre.each(function (v, i) {
@@ -123,12 +129,17 @@ class AnibatchController {
 
 
       let result = {
-        episode: {}}
+        episode: {}
+      }
       $(".dlx").find('table').each(function (v, i) {
         //if (v == 5) return
         if (!$(this).find('strong').text())
+
+
           return
-        //console.log(v)
+
+
+        // console.log($(this).find('strong').text())
 
         let eps = ''
         let table = ''
@@ -136,8 +147,8 @@ class AnibatchController {
 
         if ($(this).prev('h4') != "") {
           let h4 = $(this).prev('h4').text()
-          console.log('from h4 ')
-          console.log(h4)
+          // console.log('from h4 ')
+          // console.log(h4)
           const regex = /(?<=episode\s)[0-9]+/gmi
           // \d[^episode]
           let final_rgx = h4.match(regex)
@@ -147,56 +158,61 @@ class AnibatchController {
             eps = h4.match(regex)[0]
             const regex = /^([^~]+)/gm
 
-            eps = h4.match(regex)[0]
-            //console.log(h4.match(regex)[0])
-          } 
+            // console.log(eps)
+          }
           else if (final_rgx) {
             console.log(final_rgx[0])
             eps = final_rgx[0]
-          } 
+          }
           else {
             console.log('regex not match')
           }
-        //   //epp.push({title: 'hh'})
-        //   //console.log(epp.length)
-        //   //console.log('from h4 ' + eps)
         } else if ($(this).prev('table') != "") {
           let table = $(this).prev('table').find('b').html()
           // console.log(table)
           if (table == null) {
+            // console.log(table)
             return
-            
-          } else  {
 
-          
-
-          const regex = /(?<=episode\s)[0-9]+/gmi
-          let final_rgx = table.match(regex)
-
-          if (final_rgx == null) {
-        //     //const regex = /^(.*?[\\~])/gmi
-        //     //const regex = /^([^~]+)/gm
-
-        //     // console.log(table.match(regex)[0])
-            eps = table.match(regex)[0]
-          } else if (final_rgx) {
-            eps = final_rgx[0]
-            
-        //     //console.log(eps)
-        //     //console.log(final_rgx[0])
-
-          } else {
-            console.log('regex not match')
           }
+          else {
 
-        //   //console.log('from table ' + eps)
+
+
+            const regex = /(?<=episode\s)[0-9]+/gmi
+            let final_rgx = table.match(regex)
+            // console.log(final_rgx)
+
+            if (final_rgx == null) {
+              // console.log(final_rgx)
+
+              // console.log(table.match(regex)[0])
+              return
+              // eps = table.match(regex)[0]
+            }
+            else if (final_rgx) {
+              eps = final_rgx[0]
+
+              //     //console.log(eps)
+              //     //console.log(final_rgx[0])
+
+            }
+            else {
+              console.log('regex not match')
+            }
+
+            // // //   //console.log('from table ' + eps)
           }
-         }
+        }
+
+
+
+
         let d = {}
         let resol = {}
         let dobj = []
         let resolobj = []
-        $(this).find('tr').each(function(v, i) {
+        $(this).find('tr').each(function (v, i) {
           //console.log($(this).text())
           if ($(this).find('a') == '') {
 
@@ -212,20 +228,11 @@ class AnibatchController {
           } else {
 
             //console.log($(this).find('a').text())
-            $(this).find('a').each(function(i, v) {
+            $(this).find('a').each(function (i, v) {
               let server = $(this).text()
               let link = $(this).attr('href')
-
-              //console.log(server)
-              //console.log($(this).text())
-              //console.log($(this).find('a').attr('href'))
-              //console.log(i)
-              //console.log($(this).text())
               d[resolobj][server] = link
 
-              //result[eps] = d
-              //console.log(d['\nMp4 240p\n'])
-              //console.log(resolobj)
             })
           }
           //console.log(d)
